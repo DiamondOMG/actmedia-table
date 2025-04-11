345695ba7d9a984b8cadf006cd96756fb9817b6e
neab-dev01 <omgdigital.developer.01@gmail.com>
2025-04-11 17:33:59 +0700

    footer header between

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,20 +1,28 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
+import Header from "@/components/Layout/Header";
+import SidebarLeft from "@/components/Layout/SidebarLeft";
+import Footer from "@/components/Layout/Footer";
 
-// import layout from '@/components/Layout'
-import Header from '@/components/Layout/Header'
-import SidebarLeft from '@/components/Layout/SidebarLeft'
-import Footer from '@/components/Layout/Footer'
-
-function page() {
+function Page() {
   return (
-    <div>
-        <Header/>
-        <SidebarLeft/>
-        <Footer/>   
+    <div className="flex flex-col min-h-screen">
+      {/* Header */}
+      <Header />
+
+      {/* Main Content */}
+      <div className="flex flex-1">
+        <SidebarLeft />
+        <main className="flex-1 p-4"> {/* คอนเทนต์ตรงกลาง */}
+          {/* ใส่คอนเทนต์ของคุณที่นี่ */}
+        </main>
+      </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="w-full bg-gradient-to-b from-gray-900 to-black text-white">
+          <div className="border-t border-gray-800 py-3 text-sm text-center ps-8 ps-md-0  text-gray-400">
+          <p className="hover:text-gray-300 transition-colors">Copyright © 2025 . OMG Digital Products & Platforms 2025</p>
+        </div>
+    </footer>
   )
 }
 
