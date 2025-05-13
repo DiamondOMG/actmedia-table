2d3779426ca24cf99164bffdb372659c5caed93c
neab-dev01 <omgdigital.developer.01@gmail.com>
2025-05-13 10:31:19 +0700

    Revert "อัพเดตหน้า home + layout"
    This reverts commit d0673e2358549623079d71312ee47202e159d78b.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,28 +1,22 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 
 import Header from "@/components/Layout/Header";
 import SidebarLeft from "@/components/Layout/SidebarLeft";
 import Footer from "@/components/Layout/Footer";
 
 function Page() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen((prev) => !prev);
-  };
-
   return (
     <div className="flex flex-col min-h-screen">
       {/* Header */}
-      <Header onToggleSidebar={toggleSidebar} />
+      <Header />
 
       {/* Main Content */}
       <div className="flex flex-1">
-        <SidebarLeft  onToggle={setIsSidebarOpen} />
+        <SidebarLeft />
         <main className="flex-1 p-4"> {/* คอนเทนต์ตรงกลาง */}
           {/* ใส่คอนเทนต์ของคุณที่นี่ */}
-        </main> 
+        </main>
       </div>
 
       {/* Footer */}
